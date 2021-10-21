b1a0a0e4da8a180de8a11ae27e43cfd2245678ca
0seony <80742079+0seony@users.noreply.github.com>
2021-10-21 23:37:39 +0900

    Create 1330.java

---
+++ Baekjoon/Java/1330.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//정수 A,B 입력 받기
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A>B) {	//A가 B보다 큰 경우
+			System.out.println(">");
+		}else if(A<B) {	//A가 B보다 작은 경우
+			System.out.println("<");
+		}else {
+			System.out.println("==");
+		}
+	}
+}
