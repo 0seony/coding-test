6f99a4433b545fe87a6cfd3ce1d0a67ebfa3a387
0seony <80742079+0seony@users.noreply.github.com>
2021-10-22 22:22:45 +0900

    두 수 비교하기
    두 정수 A와 B가 주어졌을 때 두 수 를 비교하는 프로그램을 작성해라.
    -A가 B보다 큰 경우에는 '>'를 출력한다.
    -A가 B보다 작은 경우에는 '<'를 출력한다.
    -A와 B가 같은 경우에는 '=='를 출력한다.

--- Baekjoon/Java/1330.java
+++ Baekjoon/Java/1330.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		//정수 A,B 입력 받기
 		int A = sc.nextInt();
 		int B = sc.nextInt();
