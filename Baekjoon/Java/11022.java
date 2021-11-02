60512c80b4ab53de35e47be3b5db9df42eec0f14
0seony <80742079+0seony@users.noreply.github.com>
2021-11-02 15:38:49 +0900

    A+B - 8

---
+++ Baekjoon/Java/11022.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main11022 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(bf.readLine());
+
+		for (int i = 1; i <= T; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int sum = A + B;
+			
+			bw.write("Case #" + i + ": " + A + " + " + B + " = " + sum + "\n");
+		}
+		bw.flush();
+	}
+}
