a78ebc1be8020c05781402c7ead8e2aa73d7437f
0seony <80742079+0seony@users.noreply.github.com>
2021-11-02 14:20:12 +0900

    빠른 A+B

---
+++ Baekjoon/Java/15552.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main15552{
+	public static void main(String[] args)throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 테스트 케이스 입력받기
+		int T = Integer.parseInt(bf.readLine());
+		
+		// 두 정수 A,B 입력받기
+		for(int i = 0; i<T; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write(A+B + "\n");
+		}
+		bw.flush();
+	}
+}
