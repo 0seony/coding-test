39ae60aa93c0b10944b82e87e28ffb465c4f5480
0seony <80742079+0seony@users.noreply.github.com>
2021-11-04 11:28:19 +0900

    아스키 코드

---
+++ Baekjoon/Java/11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		int ase = (int)input.charAt(0);
+		System.out.print(ase);
+	}
+}
