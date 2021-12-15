db230ae1cfb836ca7a661f4a34450f0beffc0d02
0seony <80742079+0seony@users.noreply.github.com>
2021-12-15 10:58:34 +0900

    숫자의 개수

---
+++ Baekjoon/Java/2577.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main2577 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+
+		int multi = A * B * C;
+		String str_multi = Integer.toString(multi);
+
+		char[] num = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		int count = 0;
+
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < str_multi.length(); j++) {
+				if (num[i] == str_multi.charAt(j)) {
+					count++;
+				}
+			}
+			num[i] = Integer.toString(count).charAt(0);
+			count = 0;
+		}
+
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
+		}
+	}
+}
