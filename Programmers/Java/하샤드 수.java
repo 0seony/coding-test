8febaeac17a179636a9538c45ee7217d01cc4238
0seony <80742079+0seony@users.noreply.github.com>
2022-01-05 11:26:10 +0900

    하샤드 수

---
+++ Programmers/Java/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(int x) {
+        String[] arr = String.valueOf(x).split("");
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum += Integer.parseInt(arr[i]);
+        }
+        boolean answer = true;
+        if(x % sum != 0){
+            answer = false;
+        }
+        return answer;
+    }
+}
